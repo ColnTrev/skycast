5ef46796e5667c895c5423b26bb4bc0e6ee8c48f
ColnTrev <colntrev@gmail.com>
2020-09-21 02:56:32 -0700

    trying again

--- src/services/ForecastService.js
+++ src/services/ForecastService.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 export default {
     async getForecast(query){
-        let requestString;
-        if(process.env.NODE_ENV === 'production'){
-            requestString = 'http://collin-skycast-app.herokuapp.com/api/' + query;
-        } else {
-            requestString = 'http://localhost:5000/api/'+ query;
-        }
-        let res = await axios.get(requestString).then((response)=>{
+        // let requestString;
+        // if(process.env.NODE_ENV === 'production'){
+        //     requestString = 'http://collin-skycast-app.herokuapp.com/api/' + query;
+        // } else {
+        //     requestString = 'http://localhost:5000/api/'+ query;
+        // }
+        let res = await axios.get('http://collin-skycast-app.herokuapp.com/api/' + query).then((response)=>{
                 return response.data;
             }).catch((error) => {
                 return {
