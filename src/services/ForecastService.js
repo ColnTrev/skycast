dda5275ae947c3cca531be9dd83f855453ebbf8c
ColnTrev <colntrev@gmail.com>
2020-09-21 09:11:54 -0700

    restoring a screw up

--- src/services/ForecastService.js
+++ src/services/ForecastService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export default {
     async getForecast(query){
-        let res = await axios.get('/api/' + query).then((response)=>{
+        let res = await axios.get('https://skycast-api.herokuapp.com/api/' + query).then((response)=>{
                 return response.data;
             }).catch((error) => {
                 return {
